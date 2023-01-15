f189a515cc2becc3aa81a2fa0c5af41558451246
ali <ali@MacBook-Pro-Ali.local>
2023-01-15 20:56:17 +0400

    mesaj

---
+++ time span/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace time_span;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        #region Task1
+        Console.WriteLine(Ceaser.Ceaserr(1, "abcd"));
+        Console.WriteLine(Ceaser.Ceaserr(1,"abc"));
+        #endregion
+
+        #region Task2
+        //Console.WriteLine(Timespann.Timespan("13:09:21", "18:15:37"));
+        #endregion
+    }
+
+}
+
---
+++ time span/ceaser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace time_span
+{
+       public class Ceaser
+	{
+	      public static string Ceaserr(int k, string s)
+        {
+            string result = "";
+            for (int i = 0; i < s.Length; i++)
+			{
+			   result += (char)(s[i] + k);
+			}
+     
+            for (int i = 0;  i< s.Length; i++)
+            {
+                result += (char)(s[i] - k);
+            }
+            return result;
+            
+        }
+                                    
+    }
+}
+
