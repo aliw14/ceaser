fccfc73d54a51e69f38d4a581a202394eb46026c
ali <ali@MacBook-Pro-Ali.local>
2023-01-16 15:07:42 +0400

    mesaj

--- time span/Program.cs
+++ time span/Program.cs
@@ -12,8 +12,11 @@ class Program
         #region Task2
         //Console.WriteLine(Timespann.Timespan("13:09:21", "18:15:37"));
         #endregion
+        #region Task 1 davami
         Console.WriteLine(Sifrecoz.Sifrecozz(1, "bcd"));
+        #endregion
     }
+
 
 }
 
